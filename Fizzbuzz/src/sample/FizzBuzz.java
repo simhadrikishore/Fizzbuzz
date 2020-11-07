5879cc954ddb3440c85f3abaf8b3fa17f76da16c
SimhadriKishore <kishore.simhadri5@gmail.com>
2020-11-07 15:45:14 +0530

    Added output title

--- Fizzbuzz/src/sample/FizzBuzz.java
+++ Fizzbuzz/src/sample/FizzBuzz.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class FizzBuzz {
 	public static void main(String args[]) {
 		int n = 100;
+		System.out.println("Here is the fizzbuzz series:");
 		for (int i = 1; i <= n; i++) {
 			if (i % 15 == 0)
 				System.out.println("FizzBuzz" + " ");
